47d1738273d7ace89c62c960c0f8588a094e9ba4
ErshovMisha <ershov.m2908@gmail.com>
2024-05-06 01:29:01 +0300

    app

--- myapp1/urls.py
+++ myapp1/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from myapp1.views import index_page
+from myapp1.views import login_view
+from myapp1.views import generate_pdf
 
 urlpatterns = [
-    path("", index_page)
+    path("", index_page),
+    path('login/', login_view, name='login'),
+    path("pd", generate_pdf)
 ]
+
